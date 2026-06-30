b2ef28904f9ca4acaaaf6bae230484974f0a9757
uu <79706975+ImJustNon@users.noreply.github.com>
2026-07-01 01:51:14 +0700

    feat: add senior profile service dto

---
+++ apps/api/src/modules/sy-contact/dto/sy-contact-update.dto.ts
@@ -0,0 +1,49 @@
+import { ContactPlatform } from "@repo/database";
+import { Type } from "class-transformer";
+import { ArrayMaxSize, ArrayMinSize, IsArray, IsEnum, IsInt, IsNotEmpty, IsString, MaxLength, Min, ValidateNested } from "class-validator";
+
+export class ContactDto {
+	@IsEnum(ContactPlatform)
+	platform: ContactPlatform;
+
+	@IsString()
+	@IsNotEmpty()
+	@MaxLength(255)
+	value: string;
+}
+
+export class HintDto {
+	@IsInt()
+	@Min(0)
+	index: number;
+
+	@IsString()
+	@IsNotEmpty()
+	@MaxLength(255)
+	value: string;
+}
+
+export class ContactUpdateDto {
+	@IsString()
+	@IsNotEmpty()
+	@MaxLength(100)
+	firstname: string;
+
+	@IsString()
+	@IsNotEmpty()
+	@MaxLength(100)
+	lastname: string;
+
+	@IsArray()
+	@ValidateNested({ each: true })
+	@Type(() => ContactDto)
+	@ArrayMinSize(1)
+	@ArrayMaxSize(10)
+	contacts: ContactDto[];
+
+	@IsArray()
+	@ValidateNested({ each: true })
+	@Type(() => HintDto)
+	@ArrayMaxSize(20)
+	hints: HintDto[];
+}
