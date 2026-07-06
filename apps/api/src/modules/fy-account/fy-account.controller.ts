36e93aba3b5cbe2d959926ac9b91f9af5ee72351
uu <79706975+ImJustNon@users.noreply.github.com>
2026-07-06 15:10:08 +0700

    fix: lock guard for firstyear path service

--- apps/api/src/modules/fy-account/fy-account.controller.ts
+++ apps/api/src/modules/fy-account/fy-account.controller.ts
@@ -3,18 +3,21 @@ import { FyAccountService } from "./fy-account.service";
 import { LaunchPeriodGuard } from "src/common/guards/launch-period.guard";
 import { Session, type UserSession } from "@thallesp/nestjs-better-auth";
 import { LinkAccountDto } from "./dto/link-account.dto";
+import { JuniorPermission } from "src/common/guards/junior-permission.guard";
 
 @Controller("/_/fy/account")
 export class FyAccountController {
 	constructor(private readonly fyAccountService: FyAccountService) {}
 
 	@Post("/link")
+	@UseGuards(JuniorPermission)
 	@UseGuards(LaunchPeriodGuard)
 	linkFirstYearAccount(@Session() session: UserSession, @Body() linkAccountDto: LinkAccountDto) {
 		return this.fyAccountService.linkFirstYearAccount(session.user.email, linkAccountDto);
 	}
 
 	@Get("/profile")
+	@UseGuards(JuniorPermission)
 	accountProfile(@Session() session: UserSession) {
 		return this.fyAccountService.accountProfile(session.user.id);
 	}
